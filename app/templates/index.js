de6cb00020dc0d2b9809d66ce9cb044d3dc26f96
mitogh <mitogh@gmail.com>
2016-02-21 14:45:38 -0600

    Add templates file

---
+++ app/templates/index.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+
+module.exports = angular.module('templates', [])
+.run(["$templateCache", function($templateCache) {
+  $templateCache.put('home.html', require('./home/home.html'));
+  $templateCache.put('about.html', require('./about/about.html'));
+}])
