774b601b27f10fb0f0006f81da1b9193d3c3e106
adam <afenton@getmoxied.net>
2016-05-08 13:47:39 -0500

    #70 Add some default templates

---
+++ app/custom/organisms/header/directive.js
@@ -0,0 +1,14 @@
+angular
+  .module('app')
+  .directive('mxOHeader', mxOHeader);
+
+function mxOHeader() {
+  return {
+    restrict: 'A',
+    templateUrl: 'organisms/header/template.html',
+    scope: {
+      mxMenuItems: '<',
+      mxTemplate: '@'
+    }
+  };
+}
--- app/custom/templates/blog/index.js
+++ app/custom/templates/blog/index.js
@@ -1,3 +1,4 @@
 /* Add requires here for all directives referenced by the home template */
+require( './../../molecules/blog-preview/directive' );
 require( './../../organisms/header/directive' );
 require( './../../organisms/footer/directive' );
