4c71acdb9afb55f5d49af8ce8a9cf403ea41e0a6
mitogh <mitogh@gmail.com>
2016-07-12 18:40:56 -0500

    Create a directive to avoid render any content before is ready

---
+++ app/custom/organisms/loader/loader.directive.js
@@ -0,0 +1,10 @@
+angular.
+  module('app').
+  directive('lnCmsLoader', lnCmsLoader);
+
+function lnCmsLoader() {
+  return {
+    restrict: 'E',
+    templateUrl: 'organisms/loader/template.html',
+  };
+}
